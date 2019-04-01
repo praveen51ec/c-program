2fb87ee01a70519b8f277cec741c08c819e38987
praveen51ec <48515394+praveen51ec@users.noreply.github.com>
2019-04-01 16:29:55 +0530

    Rename fact.c to factorial10.c

---
+++ factorial10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{
+	int i,n;
+	char a[10],rev[10];
+	scanf("%s",a);
+	n=strlen(s);
+	for(i=n;i>=0;i--)
+{
+	static x=0;
+	rev[x]=a[i];
+	x++;
+	printf("%c",a[i]);
+}
+	return 0;
+}
