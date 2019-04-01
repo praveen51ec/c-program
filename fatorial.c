cacd67ef1aed0652a1a82a3335af87b1e0bcf295
praveen51ec <48515394+praveen51ec@users.noreply.github.com>
2019-04-01 16:11:29 +0530

    Update and rename p.c to fatorial.c

---
+++ fatorial.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{
+	int i,b;
+	char a[10],rev[10];
+	scanf("%s",s);
+	n=strlen(s);
+	for(i=b;i>=0;i--)
+{
+	static x=0;
+	rev[x]=a[i];
+	x++;
+	printf("%c",a[i]);
+}
+	return 0;
+}
